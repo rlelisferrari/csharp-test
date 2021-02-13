0426b71827230cd8082917fa3b8e65dfe2de92f8
rlelisferrari <rlelisferrari@gmail.com>
2021-02-13 17:04:39 -0300

    add controllers to all classes

---
+++ WebApi/WebApi/Controllers/OrdersController.cs
@@ -0,0 +1,18 @@
+﻿using DATA.Contexts;
+using DOMAIN.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly AppDbContext context;
+        private readonly IOrderRepository orderRepository;
+
+        public OrdersController(AppDbContext context, IOrderRepository orderRepository)
+        {
+            this.context = context;
+            this.orderRepository = orderRepository;
+        }
+    }
+}
---
+++ WebApi/WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using DATA.Contexts;
+using DOMAIN.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly AppDbContext context;
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(AppDbContext context, IProductRepository productRepository)
+        {
+            this.context = context;
+            this.productRepository = productRepository;
+        }
+    }
+}
